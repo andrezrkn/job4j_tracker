7f37a70f8067fa09d757a6901edacb37232b780c
User <vartan.andreevich@yandex.ru>
2020-07-07 19:38:35 +0300

    3. Массивы и модели. [#311874]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,32 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book first = new Book ("Clean Code", 464);
+        Book second = new Book ("Holy Bible", 1500);
+        Book third = new Book ("Koran", 900);
+        Book fourth = new Book ("Russian-English translator", 600);
+        Book[] library = new Book[4];
+        library[0] = first;
+        library[1] = second;
+        library[2] = third;
+        library[3] = fourth;
+        for (int index = 0; index < library.length; index++) {
+            Book ml = library[index];
+            System.out.println(ml.getName() + System.lineSeparator() + ml.getPagesQuantity());
+        }
+        Book mlt = library[0];
+        library[0] = library[3];
+        library[3] = mlt;
+        for (int index = 0; index < library.length; index++) {
+            Book mlx = library[index];
+            System.out.println(mlx.getName() + System.lineSeparator() + mlx.getPagesQuantity());
+        }
+        for (int index = 0; index < library.length; index++) {
+            if (library[index].getName().equals("Clean Code")) {
+                Book mlf = library[index];
+                System.out.println(mlf.getName() + System.lineSeparator() + mlf.getPagesQuantity());
+            }
+        }
+    }
+}
