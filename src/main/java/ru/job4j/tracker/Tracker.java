d7c30de0348103c4cb074ef03783fcfa47bc7f8a
User <vartan.andreevich@yandex.ru>
2020-07-08 21:46:04 +0300

    5. Tracker - хранилище [#311881]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,31 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] namesWithoutNull = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item item = items[i];
+            if (item != null) {
+                namesWithoutNull[size] = item;
+                size++;
+            }
+        }
+        namesWithoutNull = Arrays.copyOf(namesWithoutNull, size);
+        return namesWithoutNull;
+    }
+
+    public Item[] findByName(String key) {
+        int size = 0;
+        Item[] namesKey = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            if (items[i].equals(key)) {
+                namesKey[size] = items[i];
+            }
+        }
+        namesKey = Arrays.copyOf(namesKey, size);
+        return namesKey;
     }
 }
