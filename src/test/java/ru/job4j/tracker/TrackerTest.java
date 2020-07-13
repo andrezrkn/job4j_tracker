b183c62fdc962ffb6d95565c85b0bb851d231d4c
User <vartan.andreevich@yandex.ru>
2020-07-13 14:31:23 +0300

    5.1. Тестирование. Подготовка данных. [#311816]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -50,4 +50,32 @@ public class TrackerTest {
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
     }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "replaced item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("lol");
+        tracker.add(item);
+        String[] info = {
+                String.valueOf(item.getId()),
+                item.getName()
+        };
+        StartUI.deleteItem(new StubInput(info), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertThat(deleted, null);
+    }
 }
