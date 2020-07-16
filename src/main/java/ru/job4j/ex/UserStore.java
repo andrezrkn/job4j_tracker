787c8b433febd681f51a2476f681192add46025e
User <vartan.andreevich@yandex.ru>
2020-07-16 21:53:26 +0300

    0.5. Иерархия исключений и множественный catch. [#311830]

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,41 @@
+package ru.job4j.ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User user = new User("", false);
+        for (int i = 0; i < users.length; i++) {
+            if (users[i].getUsername().equals(login)) {
+                user = users[i];
+                break;
+            }
+        }
+            if (!user.isValid()) {
+                throw new UserNotFoundException();
+            }
+        return user;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || 3 > user.getUsername().length()) {
+            throw new UserInvalidException();
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Pe", false)
+        };
+        try {
+            User user = findUser(users, "Pe");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException ui) {
+            ui.printStackTrace();
+        } catch (UserNotFoundException un) {
+            un.printStackTrace();
+        }
+    }
+}
