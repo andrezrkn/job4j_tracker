8683df6829869771979ba18e013f605cd6a755a8
User <vartan.andreevich@yandex.ru>
2020-07-17 14:01:24 +0300

    1.1. Тесты на StartUI. [#311832]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -257,4 +257,22 @@ public class TrackerTest {
 
         ));
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                        "Menu" + System.lineSeparator()
+                                + "0. === Exit ====" + System.lineSeparator()
+
+        ));
+    }
 }
