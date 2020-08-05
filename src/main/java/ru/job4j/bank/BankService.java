f191728aebed2c4e000e24ad6817ef619fb4985e
User <vartan.andreevich@yandex.ru>
2020-08-05 19:59:52 +0300

    3. Банковские переводы [#311796]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,109 @@
+package ru.job4j.bank;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        boolean rsl = true;
+        for (User element : users.keySet()) {
+            if (element.getPassport().equals(user.getPassport())
+                    && element.getUsername().equals(element.getUsername())) {
+                rsl = false;
+                break;
+            }
+        }
+        if (rsl) {
+            users.put(user, null);
+        }
+    }
+
+    public void addAccount(String passport, Account account) {
+        User citizen = findByPassport(passport);
+
+    }
+
+    public User findByPassport(String passport) {
+        User citizen = null;
+        for (User element : users.keySet()) {
+            if (element.getPassport().equals(passport)) {
+                citizen = element;
+                break;
+            }
+        }
+        return citizen;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        Account rsl = null;
+        User user = null;
+        for (Map.Entry<User, List<Account>> element : users.entrySet()) {
+            if (element.getKey().getPassport() == passport) {
+                user = element.getKey();
+                break;
+            }
+        }
+        if (user != null) {
+            List<Account> req = new ArrayList<>();
+            for (Account element : users.get(user)) {
+                req.add(element);
+            }
+            for (Account element : req) {
+                if (element.getRequisite() == requisite) {
+                    rsl = element;
+                    break;
+                }
+            }
+        }
+        return rsl;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        User srcUser, destUser;
+        Account srcAccount, destAccount;
+        srcUser = destUser = null;
+        srcAccount = destAccount = null;
+        for (Map.Entry<User, List<Account>> element : users.entrySet()) {
+            if (element.getKey().getPassport() == srcPassport) {
+                srcUser = element.getKey();
+                break;
+            }
+        }
+        if (srcUser != null) {
+            List<Account> mass = users.get(srcUser);
+            for (Account element : mass) {
+                if (element.getRequisite() == srcRequisite) {
+                    srcAccount = element;
+                    break;
+                }
+            }
+            if (srcAccount.getBalance() - amount >= 0) {
+                for (Map.Entry<User, List<Account>> element : users.entrySet()) {
+                    if (element.getKey().getPassport() == destPassport) {
+                        destUser = element.getKey();
+                        break;
+                    }
+                }
+                if (destUser != null) {
+                    mass = users.get(srcUser);
+                    for (Account element : mass) {
+                        if (element.getRequisite() == destRequisite) {
+                            destAccount = element;
+                            srcAccount.setBalance(srcAccount.getBalance() - amount);
+                            destAccount.setBalance(destAccount.getBalance() + amount);
+                            rsl = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return rsl;
+    }
+}
