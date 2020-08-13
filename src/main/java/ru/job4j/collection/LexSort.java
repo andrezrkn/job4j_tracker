9e3f7f4e4b73a9e39244477781141e606ae7c8ce
User <vartan.andreevich@yandex.ru>
2020-08-13 18:29:25 +0300

    4. Сортировка номера [#311781]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -6,22 +6,9 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        int rsl = 0;
         String[] l = left.split("\\.");
         String[] r = right.split("\\.");
         int length = Math.min(l.length, r.length);
-        for (int i = 0; i < length; i++) {
-            if (Integer.parseInt(l[i]) < Integer.parseInt(r[i])) {
-                rsl = -1;
-                break;
-            } else if (Integer.parseInt(l[i]) > Integer.parseInt(r[i])) {
-                rsl = 1;
-                break;
-            }
-        }
-        if (rsl == 0) {
-            rsl = Integer.compare(l.length, r.length);
-        }
-        return rsl;
+        return Integer.compare(Integer.parseInt(l[0]), Integer.parseInt(r[0]));
     }
 }
