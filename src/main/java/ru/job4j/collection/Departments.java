0f0636558bbc003aaba80a7a816ae9762dadeaae
User <vartan.andreevich@yandex.ru>
2020-08-31 20:27:56 +0300

    Отсортировать департаменты [#311777]

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -1,14 +1,11 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Departments {
 
     public static List<String> fillGaps(List<String> deps) {
-        HashSet<String> tmp = new HashSet<>();
+        TreeSet<String> tmp = new TreeSet<>();
         for (String value : deps) {
             String  start = "";
             for (String el : value.split("/")) {
