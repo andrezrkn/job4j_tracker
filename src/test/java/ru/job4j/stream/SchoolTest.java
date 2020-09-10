77e4dd1e0a3206735d04b22271619b94d4caef21
User <vartan.andreevich@yandex.ru>
2020-09-10 12:19:55 +0300

    1. Фильтрация учеников [#311639]

--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -2,63 +2,57 @@ package ru.job4j.stream;
 
 import org.junit.Test;
 import java.util.List;
+import java.util.function.Predicate;
 import static org.junit.Assert.*;
-
-@FunctionalInterface
-interface MyPredicate {
-    boolean test(Integer value);
-}
 
  public class SchoolTest {
 
     @Test
     public void whenAClass() {
-        MyPredicate myPredicate = x -> x >= 70 && x <= 100;
+        School school = new School();
+        Predicate<Student> predicate = Student -> Student.getScore() >= 70
+                && Student.getScore() <= 100;
         List<Student> mass = List.of(
                 new Student("1", 40),
                 new Student("2", 60),
                 new Student("3", 80)
                 );
-        int count = 0;
-        for (Student element : mass) {
-           if  (myPredicate.test(element.getScore())) {
-               count++;
-           }
-        }
-        assertEquals(1, count);
+        List<Student> expected = List.of(
+                new Student("3", 80)
+        );
+        assertEquals(school.collect(mass, predicate), expected);
+
     }
 
     @Test
     public void whenBClass() {
-        MyPredicate myPredicate = x -> x >= 50 && x < 70;
+        School school = new School();
+        Predicate<Student> predicate = Student -> Student.getScore() >= 50
+                && Student.getScore() < 70;
         List<Student> mass = List.of(
                 new Student("1", 40),
                 new Student("2", 60),
                 new Student("3", 80)
         );
-        int count = 0;
-        for (Student element : mass) {
-            if  (myPredicate.test(element.getScore())) {
-                count++;
-            }
-        }
-        assertEquals(1, count);
+        List<Student> expected = List.of(
+                new Student("2", 60)
+        );
+        assertEquals(school.collect(mass, predicate), expected);
     }
 
     @Test
     public void whenCClass() {
-        MyPredicate myPredicate = x -> x > 0 && x < 50;
+        School school = new School();
+        Predicate<Student> predicate = Student -> Student.getScore() > 0
+                && Student.getScore() < 50;
         List<Student> mass = List.of(
                 new Student("1", 40),
                 new Student("2", 60),
                 new Student("3", 80)
         );
-        int count = 0;
-        for (Student element : mass) {
-            if  (myPredicate.test(element.getScore())) {
-                count++;
-            }
-        }
-        assertEquals(1, count);
+        List<Student> expected = List.of(
+                new Student("1", 40)
+        );
+        assertEquals(school.collect(mass, predicate), expected);
     }
 }
