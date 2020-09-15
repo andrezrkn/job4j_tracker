f1a4e6a70bfd8d1c4f3611ba36da7dc87f52c8e1
User <vartan.andreevich@yandex.ru>
2020-09-15 14:07:19 +0300

    4. Преобразование List в Map. [#311642]

--- src/test/java/ru/job4j/stream/ListInMapTest.java
+++ src/test/java/ru/job4j/stream/ListInMapTest.java
@@ -12,7 +12,6 @@ import static org.hamcrest.Matchers.is;
 public class ListInMapTest {
     @Test
     public void whenNoSame() {
-        AtomicReference<Integer> id = new AtomicReference<>(0);
         List<Student> mass = List.of(
                 new Student("1", 98),
                 new Student("1", 99),
@@ -23,19 +22,13 @@ public class ListInMapTest {
                         Collectors.toMap(
                                 Student::getSurname,
                                 Student -> Student,
-                                (a, b) -> {
-                                   id.set(id.get() + 1);
-                                   a.setSurname(a.getSurname() + id.toString());
-                                   return a;
-                                }
+                                (a, b) -> a
                         )
                 );
         Student m1 = new Student("1", 98);
-        Student m2 = new Student("11", 99);
         Student m3 = new Student("3", 100);
         Map expected = new HashMap();
         expected.put(m1.getSurname(), m1);
-        expected.put(m2.getSurname(), m2);
         expected.put(m3.getSurname(), m3);
         assertThat(st, is(expected));
     }
