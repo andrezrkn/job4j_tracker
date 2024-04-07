ee215d6563de73610ae513037f81c0d1f8d0766c
andrezrkn <vartan.andreevich@yandex.ru>
2024-04-07 15:00:49 +0300

    1. Singleton [#94606]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,7 +1,42 @@
 package ru.job4j.tracker;
 
-public class SingleTracker {
-    private Tracker tracker = new Tracker();
+import java.util.List;
 
+public final class SingleTracker {
 
+    private static Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+    }
+
+    public static Tracker getTracker() {
+        if (tracker == null) {
+            tracker = new Tracker();
+        }
+        return tracker;
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public List<Item> findAll() {
+        return tracker.findAll();
+    }
+
+    public List<Item> findByName(String key) {
+        return tracker.findByName(key);
+    }
 }
